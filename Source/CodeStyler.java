dfd50bbb1d900255fa7471c87e727d5b50ca0980
MatheusBack-end <email.matheussilva2@gmail.com>
2023-12-05 13:22:54 -0300

    its work =]

--- Source/CodeStyler.java
+++ Source/CodeStyler.java
@@ -3,13 +3,10 @@ package JAVARuntime;
 public class CodeStyler extends TextScriptingStyler
 {
     int position = 0;
-    String source;
     ThemeLoader themeConfig;
     List<String> types = new ArrayList<String>();
     private boolean typesLoaded = false;
     private Tokenizer tokenizer;
-    int nonBlocking = 0;
-    int nonBlockingLimit = 4000;
     
     public CodeStyler(ThemeLoader config) {
         themeConfig = config;
@@ -17,196 +14,61 @@ public class CodeStyler extends TextScriptingStyler
     }
     
     @Override
-    public void execute(String src, TextScriptingTheme theme, List<TextScriptingSyntaxHighlightSpan> highlight_spans) {
-        nonBlocking = 0;
-               
+    public void execute(String source, TextScriptingTheme theme, List<TextScriptingSyntaxHighlightSpan> highlight_spans) {
         collectNames();
-        this.source = src;
+        
+        highlight_spans.add(createHighlight(new Point2(0, source.length()), theme.textColor));
         
         tokenizer = new Tokenizer(source);
         Token currentToken = tokenizer.getNextToken();
         
         while(!currentToken.type.equals("eof")) {
-            if(nonBlocking >= nonBlockingLimit)
-                break;
+            if(currentToken.type.equals("identifier")) {
+                if(themeConfig.is_keyword(currentToken.value)) {
+                    highlight_spans.add(createHighlight(new Point2(currentToken.start, currentToken.end), theme.keywordColor));
+                }
+            }
             
-            Console.log(currentToken.type + " -> " + currentToken.value);
+            if(currentToken.type.equals("string")) {
+                 highlight_spans.add(createHighlight(new Point2(currentToken.start, currentToken.end + 1), theme.stringColor));
+                 applyStringColors(currentToken.start, 0, currentToken.value.length(), currentToken.value, highlight_spans);
+            }
+            
+            if(currentToken.type.equals("comment")) {
+                highlight_spans.add(createHighlight(new Point2(currentToken.start, currentToken.end), theme.commentColor));
+            }
+            
             currentToken = tokenizer.getNextToken();
-            nonBlocking++;
         }
-        
-        highlight_spans.add(createHighlight(new Point2(0, src.length()), theme.textColor));
-        
-        while(position < source.length()) {
-            String letter = Character.toString(source.charAt(position));
-            
-            if((letter.equals(" ")) || (letter.equals("\n"))) {
-                position++;
-                continue;
-            }
-
-            if(Character.isLetterOrDigit(letter.charAt(0))) {
-                int start = position;
-                String value = "";
-
-                while(Character.isLetterOrDigit(letter.charAt(0))) {
-                    value += letter;
-                    
-                    if(position >= source.length() - 1)
-                        break;
-
-                    letter = consumeLetter();
-                }
-                
-                if(types.contains(value)) {
-                    highlight_spans.add(createHighlight(new Point2(start, position), theme.typeColor));
-                    
-                    continue;
-                }
-                
-                if(themeConfig.is_keyword(value)) {
-                    highlight_spans.add(createHighlight(new Point2(start, position), theme.keywordColor));
-                    
-                    continue;
-                }
-                
-                highlight_spans.add(createHighlight(new Point2(start, position), theme.textColor));
-            }
-            
-            if(letter.equals("\"")) {
-                int start = position;
-                
-                if(position < source.length() - 1)
-                    letter = consumeLetter();
-                
-                while(!letter.equals("\"")) {
-                    if(letter.equals("\\")) {
-                        letter = consumeLetter();
-                        
-                        if(letter.equals("\"")) {
-                            letter = consumeLetter();
-                            continue;
-                        }
-                        
-                        continue;
-                    }
-                    
-                    if(position >= source.length() - 1) break;
-                    
-                    
-                    letter = consumeLetter();
-                }
-                
-                highlight_spans.add(createHighlight(new Point2(start, position + 1), theme.stringColor));
-                applyStringColors(start, position, highlight_spans);
-            }
-            
-            applyComments(theme, highlight_spans);
-            
-            position++;
-        }
-        
-        position = 0;
     }
     
-    private void applyStringColors(int offset, int end, List<TextScriptingSyntaxHighlightSpan> highlight_spans)
+    private void applyStringColors(int offset, int start, int end, String string, List<TextScriptingSyntaxHighlightSpan> highlightSpans)
     {
         String letter = "";
         
-        for(; offset < end; offset++) {
+        for(; start < end; start++) {
             if(letter.equals("#")) {
-                int color_start = offset;
+                int color_start = start;
                 String color = letter;
                 
-                letter = Character.toString(source.charAt(offset++));
+                letter = Character.toString(string.charAt(start++));
 
                 for(int i = 0; i < 6; i++) {
-                    if(offset > end)
+                    if(start >= end)
                         break;
                         
                     color += letter;
                     
-                    letter = Character.toString(source.charAt(offset++));
+                    letter = Character.toString(string.charAt(start++));
                 }
                 
                 if(color.length() == 7) {
-                    highlight_spans.add(createHighlight(new Point2(color_start -1, offset -1), new Color(color)));
+                    highlightSpans.add(createHighlight(new Point2((color_start + offset) -1, (start + offset)), new Color(color)));
                 }
             }
             
-            letter = Character.toString(source.charAt(offset));
+            letter = Character.toString(string.charAt(start));
         }
-    }
-    
-    public void applyComments(TextScriptingTheme theme, List<TextScriptingSyntaxHighlightSpan> highlightSpans) {
-        String letter = getCurrentLetter();
-        int start = position;
-        
-        if(Character.isLetterOrDigit(letter.charAt(0)))
-            return;
-            
-        String unkOperator = eatOperators(2, false);
-        if(unkOperator == null)
-            return;
-        
-        if(unkOperator.equals("//")) {
-            eatOperators(2, true);
-            while(!letter.equals("\n")) {
-                letter = consumeLetter();
-            }
-            
-            highlightSpans.add(createHighlight(new Point2(start, position), theme.commentColor));
-        }
-            
-        if(unkOperator.equals("/*")) {
-            eatOperators(2, true);
-            while(true) {
-                if(!Character.isLetterOrDigit(letter.charAt(0))) {
-                    if(eatOperators(2, true).equals("*/"))
-                        break;
-                }
-                    
-                if(position >= source.length() - 1)
-                    break;
-                    
-                letter = consumeLetter();
-            }
-            
-            highlightSpans.add(createHighlight(new Point2(start, position), theme.commentColor));
-        }
-    }
-    
-    public String eatOperators(int size, boolean consume) {
-        String operator = "";
-        
-        if(position >= source.length())
-            return null;
-        
-        String letter = getCurrentLetter();
-        int count = 0;
-        int offset = position;
-        
-        while(!Character.isLetterOrDigit(letter.charAt(0))) {
-            if(letter.equals(" "))
-                break;
-                
-            if(count >= size)
-                break;
-                    
-            operator += letter;
-            
-            if(position >= source.length() - 1 || offset >= source.length() - 1)
-                break;
-            
-            if(consume)
-                letter = consumeLetter();
-            else
-                letter = Character.toString(source.charAt(++offset));
-            
-            count++;
-        }
-        
-        return operator;
     }
     
     private TextScriptingSyntaxHighlightSpan createHighlight(Point2 location, Color color) {
@@ -218,14 +80,6 @@ public class CodeStyler extends TextScriptingStyler
         highlight.span = span;
         
         return highlight;
-    }
-    
-    private String consumeLetter() {
-        return Character.toString(source.charAt(++position));
-    }
-    
-    private String getCurrentLetter() {
-        return Character.toString(source.charAt(position));
     }
     
     private void collectNames() {
